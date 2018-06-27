76ca30b5c88a5a413f685affbf51dee6a6692e98
Robert Jacob <jacob@robjacob.home>
2018-06-27 10:37:06 -0400

    implemented simple filter

--- filter.py
+++ filter.py
@@ -1,8 +1,8 @@
 # Apply filter to stream of raw numerical brain data
 
-# This is currently a no-op placeholder
-# for a simple low pass/moving average filter.
-# But if it's better to do the filtering inside of neuracle, can forget about this.
+# This is a simple low pass/moving average or box filter.
+# For more sophisticated filters, can use numpy.convolve()
+# If it's better to do the filtering inside of neuracle, can forget about this.
 
 # Send each new multidimensional data point to us (as a list of floats)
 # we send back the appropriate new point to use.
@@ -16,6 +16,7 @@ class MAvgFilter:
 		self.nwindow = 5
 
 		# Saves as much recent input data as we'll use,
+		# array of multi-dimensional data points,
 		# latest value at beginning
 		self.data = []
 
@@ -26,8 +27,10 @@ class MAvgFilter:
 		# BTW this creates new copy, can use "del" to delete in place
 		self.data = self.data[:self.nwindow]
 
-		# Does nothing for now
-		return inp
+		# Mean value for each dimension,
+		# in slighly weird semi-functional programming style.
+		# Can also do this easily with numpy.
+		return [ float(sum(l)) / len(l) for l in zip(*self.data) ]
 
 #
 # SIMPLE UNIT TEST
@@ -38,4 +41,20 @@ if __name__=="__main__":
 
 	myfilter = MAvgFilter()
 
-	# Not implemented
+	inp = [.0, 0, 0, 0, 0] ; print (inp, "->", myfilter.process(inp))
+	inp = [.0, 0, 0, 0, 0] ; print (inp, "->", myfilter.process(inp))
+	inp = [.4, .1, 0, 0, 0] ; print (inp, "->", myfilter.process(inp))
+	inp = [.4, 0, .2, 0, 0] ; print (inp, "->", myfilter.process(inp))
+	inp = [.4, 0, 0, .3, 0] ; print (inp, "->", myfilter.process(inp))
+	inp = [.4, 0, 0, 0, .5] ; print (inp, "->", myfilter.process(inp))
+	inp = [.4, 0, .2, 0, 0] ; print (inp, "->", myfilter.process(inp))
+	inp = [.4, .1, 0, 0, 0] ; print (inp, "->", myfilter.process(inp))
+	inp = [.4, 0, .2, 0, 0] ; print (inp, "->", myfilter.process(inp))
+	inp = [.4, .1, 0, 0, 0] ; print (inp, "->", myfilter.process(inp))
+	inp = [.4, 0, .2, 0, 0] ; print (inp, "->", myfilter.process(inp))
+	inp = [.4, .1, 0, 0, 0] ; print (inp, "->", myfilter.process(inp))
+	inp = [.4, 0, .2, 0, 0] ; print (inp, "->", myfilter.process(inp))
+	inp = [.4, .1, 0, 0, 0] ; print (inp, "->", myfilter.process(inp))
+	inp = [.4, 0, .2, 0, 0] ; print (inp, "->", myfilter.process(inp))
+	inp = [.4, .1, 0, 0, 0] ; print (inp, "->", myfilter.process(inp))
+	inp = [.4, 0, .2, 0, 0] ; print (inp, "->", myfilter.process(inp))
